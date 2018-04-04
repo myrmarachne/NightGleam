62dfd0752f26c57b2d91f2452b93628f243b91ea
Karolina <cynk.karolina@gmail.com>
2018-04-04 23:55:22 +0200

    Added dynamic opacity of spell + its basic movement

--- Assets/SpellController.cs
+++ Assets/SpellController.cs
@@ -10,19 +10,37 @@ public class SpellController : MonoBehaviour {
      * (1) maxymalne bounciness + ograniczenia na velocity wzdluz x oraz wzdluz y
      * (2) "wyskok" zaklecia - z wysokosci playera (wziac pod uwage np przy wyskoku itp)
      * 
+     * niech tuz przed kolizja z ziemia - stala szybkosc
      */
 
     private float maxVelocityX = 1;
     private float maxVelocityY = 1;
 
     private float collisionPadding = 0.01f;
+    private int groundCollisionsCounter;
+    private int maxGroundCollisions = 5;
 
-    private Vector2 velocity = new Vector2(1,0);
+    private float maxSeconds = 5f;
+
+    private bool startVanishing;
+
+    private Vector2 velocity;
     Rigidbody2D rbody;
+    SpriteRenderer renderer;
 
     private void Awake() {
+        velocity = new Vector2(2, 3);
+        groundCollisionsCounter = 0;
+
+        renderer = GetComponent<SpriteRenderer>();
+        Color textureColor = renderer.color;
+        textureColor.a = 1f;
+        renderer.color = textureColor;
+
         rbody = GetComponent<Rigidbody2D>();
         rbody.velocity = velocity;
+
+
     }
 
     private void Start () {
@@ -34,9 +52,46 @@ public class SpellController : MonoBehaviour {
 	}
 
     private void FixedUpdate() {
+        Color textureColor = renderer.color;
+        textureColor.a -= (Time.deltaTime/maxSeconds);
+        renderer.color = textureColor;
 
-       
-
+        if (textureColor.a <= 0f) {
+            this.gameObject.SetActive(false);
+            Destroy(this);
+        }
     }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+
+        if (collision.collider.name == "Ground") {
+            /* Set velocity before collision with the floor */
+            rbody.velocity = velocity;
+
+          //  groundCollisionsCounter++;
+
+            /* Make the bullet more transparent in each of the collision
+             * with the ground */
+
+           // Color textureColor = renderer.color;
+           // textureColor.a -= (1f / maxGroundCollisions);
+            //renderer.color = textureColor;
+
+
+          //  if (groundCollisionsCounter >= maxGroundCollisions) {
+            //    startVanishing = true;
+           //     this.gameObject.SetActive(false);
+           //     Destroy(this);
+          //  }
+        } else {
+            /*
+             * TODO: Handle collision with enemies             
+             */
+            this.gameObject.SetActive(false);
+            Destroy(this);
+        }
+    }
+
+
 
 }
